7e8ec1599ff935b2474d76519362aa5db8370ca6
larrykas <larrykas@users.noreply.github.com>
2013-10-18 18:24:39 +0300

    Create if_elsif_else.rb
    if_elsif_else

---
+++ if_elsif_else.rb
@@ -0,0 +1,28 @@
+###################################################
+# Name a fruit: if_elsif_else
+###################################################
+puts "Name a fruit:"
+fruit = gets.chomp
+      if fruit == "orange"
+       puts "The color is orange."
+      elsif fruit == "apple"
+       puts "The color is green."
+      elsif fruit == "banana"
+       puts "The color is yellow."
+      else
+       puts "I don't know the color of this fuit."
+      end
+      
+###################################################
+# What is your name? If_ elsif_else
+###################################################      
+puts " What is your name?"
+name = gets.chomp # chomp gets rid of any white space.
+if name === "Larry"
+  puts "Bienvenue Larry! Que puis-je faire pour vous aujourd'hui? "
+
+elsif name === "Jenny"
+  puts "Welcome Jenny! What can I do for you today?"
+else
+  puts "I don't think we've met."
+end
